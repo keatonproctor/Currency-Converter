3ac7a0c56660287802834b9be688624f8a84d83f
Keaton Proctor <keatonproctor@Keatons-MacBook-Pro.local>
2020-08-06 10:23:20 -0600

    Connected to Currency API

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import CurrencyRow from './CurrencyRow';
+
+const BASE_URL = 'https://api.exchangeratesapi.io/latest'
 
 function App() {
+  const [currencyOptions, setCurrencyOptions] = useState([])
+  console.log(currencyOptions);
+  
+  useEffect(() => {
+    fetch(BASE_URL)
+      .then(res => res.json())
+      .then(data => {
+        setCurrencyOptions([data.base, ...Object.keys(data.rates)])
+      })
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Convert</h1>
+      <CurrencyRow 
+        currencyOptions={currencyOptions}
+      />
+      
+      <div className="equals">=</div>
+      <CurrencyRow 
+        currencyOptions={currencyOptions}
+      />
+    </>
   );
 }
 
